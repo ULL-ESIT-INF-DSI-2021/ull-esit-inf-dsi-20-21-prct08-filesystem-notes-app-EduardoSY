16e0dbf841199562bf1c93be336b923fce89aaa9
Eduardo Da Silva <alu0101104911@ull.edu.es>
2021-04-25 13:02:10 +0100

    Avances en la app + tests

--- src/noteApp/userNoteOptions.ts
+++ src/noteApp/userNoteOptions.ts
@@ -1,18 +1,68 @@
-import {Note} from './note'
+import {Note} from './note';
+import * as fs from 'fs';
+import * as chalk from 'chalk';
+
 
 export class UserNoteOptions {
   constructor() {}
 
-  addNote(nota: Note): void {
+  addNote(usuario: string, titulo: string, cuerpo: string,
+      color: string): void {
+    if (fs.existsSync(`db/${usuario}`) == false) {
+      console.log('Creado fichero del usuario');
+      fs.mkdirSync(`db/${usuario}`, {recursive: true});
+    }
+    const nota = new Note(titulo, cuerpo, color);
+    if (fs.existsSync(`db/${usuario}/${titulo}.json`) == false) {
+      fs.writeFileSync(`db/${usuario}/${titulo}.json`, nota.noteToJSON());
+      console.log(chalk.green('Nota creada correctamente!'));
+    } else {
+      console.log(chalk.red('ERROR: Parece que ya existe'+
+          ' una nota con el mismo titulo'));
+    }
   }
 
-  removeNote():void {
-
+  removeNote(usuario: string, titulo: string): void {
+    if (fs.existsSync(`db/${usuario}/${titulo}.json`) == true) {
+      fs.rmSync(`db/${usuario}/${titulo}.json`);
+      console.log(chalk.green('Nota eliminada correctamente!'));
+    } else {
+      console.log(chalk.red('ERROR: Parece que esa nota no existia'));
+    }
   }
 
-  modifyNote():void {
-
+  modifyNote(usuario: string, titulo: string, cuerpo: string,
+      color: string): void {
+    if (fs.existsSync(`db/${usuario}/${titulo}.json`) == true) {
+      const nota = new Note(titulo, cuerpo, color);
+      fs.writeFileSync(`db/${usuario}/${titulo}.json`, nota.noteToJSON());
+      console.log(chalk.green('Nota modificada correctamente!'));
+    } else {
+      console.log(chalk.red('ERROR: Parece que esa nota no existia'));
+    }
   }
 
-  
+
+  listNotes(usuario: string): Note[] {
+    let listNotes: Note[] = [];
+    fs.readdirSync(`db/${usuario}`).forEach((notes) => {
+      const info = fs.readFileSync(`db/${usuario}/${notes}`);
+      const notaJson = JSON.parse(info.toString());
+      const nota = new Note(notaJson.title, notaJson.body, notaJson.color);
+      listNotes.push(nota);
+    });
+    return listNotes;
+  }
+
+  readNote(usuario: string, titulo: string): void {
+    if (fs.existsSync(`db/${usuario}/${titulo}.json`) == true) {
+      const info = fs.readFileSync(`db/${usuario}/${titulo}.json`);
+      const notaJson = JSON.parse(info.toString());
+      const nota = new Note(notaJson.title, notaJson.body, notaJson.color);
+      console.log(chalk.keyword(nota.getColor())(nota.getTitle()));
+      console.log(chalk.keyword(nota.getColor())(nota.getBody()));
+    } else {
+      console.log(chalk.red('ERROR: Parece que esa nota no existia'));
+    }
+  }
 }
---
+++ tests/noteApp_test/userNoteOptions.spec.ts
@@ -0,0 +1,40 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Note} from '../../src/noteApp/note';
+import {UserNoteOptions} from '../../src/noteApp/userNoteOptions';
+import * as fs from 'fs';
+
+describe('Test userNoteOptions', () => {
+  let userOpt = new UserNoteOptions();
+  it('Se puede crear una nueva nota', () => {
+    userOpt.addNote('Test', 'Nota_test', 'Esta es una nota de prueba', 'green');
+    expect(fs.existsSync('db/Test/Nota_test.json')).true;
+  });
+
+  it('Se puede modificar una nota', () => {
+    userOpt.modifyNote('Test', 'Nota_test',
+        'Esta es una nota de prueba modificada', 'blue');
+    expect(fs.existsSync('db/Test/Nota_test.json')).true;
+    let info = fs.readFileSync('db/Test/Nota_test.json');
+    expect(info.toString()).to.be.eql('{\n\"title\": \"Nota_test' +
+    '\",\n\"body\": \"Esta es una nota de prueba modificada'+
+    '\",\n\"color\": \"blue' + '\"\n}');
+  });
+
+  it('Se puede listar las notas de un usuario', () => {
+    userOpt.addNote('Test', 'Nota_test2', 'Esta es una nota de prueba',
+        'green');
+
+    let nota1 = new Note('Nota_test',
+        'Esta es una nota de prueba modificada', 'blue');
+    let nota2 = new Note('Nota_test2', 'Esta es una nota de prueba',
+        'green');
+    expect(userOpt.listNotes('Test')).to.be.eql([nota1, nota2]);
+  });
+
+  it('Se puede eliminar una nota', () => {
+    userOpt.removeNote('Test', 'Nota_test');
+    expect(fs.existsSync('db/Test/Nota_test.json')).false;
+  });
+});
+
